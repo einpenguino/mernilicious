3205ad6e0e09f5b496970dae45ec014c257994b7
einpenguino <youyuneugene@gmail.com>
2022-12-18 10:34:54 +0800

    auth token exchange update

--- services/generateToken.js
+++ services/generateToken.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "30d",
   });
 };
 
